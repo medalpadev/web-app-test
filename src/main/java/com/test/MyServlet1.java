97cc66bf4a6a1db6ff248f9b1de7f831574064ba
owndev <med.amd@gmail.com>
2015-04-26 19:01:19 +0200

    first commit

---
+++ src/main/java/com/test/MyServlet1.java
@@ -0,0 +1,28 @@
+package com.test;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class MyServlet1 extends HttpServlet {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		response.setContentType("text/html");
+		String param = request.getParameter("param1");
+		PrintWriter out = response.getWriter();
+		out.println("<h1>Prama1= < "+param+" >");
+		out.close();
+	}
+
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,11 @@
+<html>
+<body>
+<h2>Form !</h2>
+<form action="/MyWebApp1/login" method="post">
+	<input type="text" name="param1" value="" size="15">
+	<input type="password" name="password2" size=10 value="" maxlength=10>
+	<input type="submit" value="Submit"> 
+	<input type="reset" value="Reset">
+</form>
+</body>
+</html>
